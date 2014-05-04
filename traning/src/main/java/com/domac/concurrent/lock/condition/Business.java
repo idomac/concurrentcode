46684a28aa87836eee4424de8dfbd4a2d07816cb
quanet <quanet@163.com>
2014-05-04 15:01:20 +0800

    提交代码

--- traning/src/main/java/com/domac/concurrent/lock/condition/Business.java
+++ traning/src/main/java/com/domac/concurrent/lock/condition/Business.java
@@ -19,10 +19,14 @@ public class Business {
      * 主方法
      */
     public void manFun(int loop) throws InterruptedException {
+        System.out.println("........................... main lock1");
         lock.lock();
+        System.out.println("........................... main lock2");
         try {
             while (bool) {
+                System.out.println("--------------------------------------------main fun await1");
                 condition.await();//this.wait();
+                System.out.println("--------------------------------------------main fun await2");
             }
 
             for(int i = 0; i< 100; i++) {
@@ -31,6 +35,7 @@ public class Business {
             bool = true;
             condition.signal();//this.notify();
         }finally {
+            System.out.println("........................... main lock un");
             lock.unlock();
         }
     }
@@ -39,10 +44,14 @@ public class Business {
      * 子方法
      */
     public void subFun(int loop) throws InterruptedException {
+        System.out.println("........................... sub lock1");
         lock.lock();
+        System.out.println("........................... sub lock2");
         try {
             while (!bool) {
+                System.out.println("--------------------------------------------sub fun await1");
                 condition.await();//this.wait;
+                System.out.println("--------------------------------------------sub fun await2");
             }
 
             for(int i = 0; i<10;i++) {
@@ -51,6 +60,7 @@ public class Business {
             bool = false;
             condition.signal();//this.notify()
         }finally {
+            System.out.println("........................... sub lock un");
             lock.unlock();
         }
     }
