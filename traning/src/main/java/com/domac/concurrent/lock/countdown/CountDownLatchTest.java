515078ca24882d33e2d77d5d9a7683f09302f5b0
quanet <quanet@163.com>
2014-05-04 11:37:17 +0800

    提交代码

--- traning/src/main/java/com/domac/concurrent/lock/countdown/CountDownLatchTest.java
+++ traning/src/main/java/com/domac/concurrent/lock/countdown/CountDownLatchTest.java
@@ -1,4 +1,4 @@
-package com.domac.concurrent.lock.CountDownLatch;
+package com.domac.concurrent.lock.countdown;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
